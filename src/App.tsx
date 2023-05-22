0f825dcbfb490691482588d40316ac29cda190a0
Алихан Эльдерханов <alihanelderhanov@MacBook-Air.local>
2023-05-23 01:58:18 +0300

    deleted empty component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Provider } from 'react-redux';
 import {store} from './app/store';
 import Header from './components/Header';
-import Main from './components/Main';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className="app">
         <Header />
-        <Main />
       </div>
     </Provider>
   );
